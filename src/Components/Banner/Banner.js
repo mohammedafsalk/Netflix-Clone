c1429292f7188163014ffc43ba17a11e5b55a074
mohammedafsalk <techafs456@gmail.com>
2023-05-22 19:44:24 +0530

    axios added

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,20 +1,39 @@
-import React from 'react'
-import './Banner.css'
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import { API_KEY, image } from "../../constants/constants";
+import axios from "../../axios";
 
 function Banner() {
+  const [item, setItem] = useState();
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
+      )
+      .then((response) => {
+        let index = Math.floor(Math.random() * response.data.results.length);
+        setItem(response.data.results[index]);
+      });
+  }, []);
   return (
-    <div className='banner'>
-        <div className="content">
-            <h1 className="title">Movie Name</h1>
-            <div className="banner_buttons">
-                <button className="button">Play</button>
-                <button className="button">My List</button>
-            </div>
-            <h1 className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odio quis ad odit praesentium itaque dolores vel! Velit sapiente veniam pariatur sunt recusandae id dignis</h1>
+    <div
+      className="banner"
+      style={{
+        backgroundImage: `url(${item ? image + item.backdrop_path : ""})`,
+      }}
+    >
+      <div className="content">
+        <h1 className="title">{item ? item.title : ""}</h1>
+        <div className="banner_buttons">
+          <button className="button">Play</button>
+          <button className="button">My List</button>
         </div>
-        <div className="fade_bottom"></div>
+        <h1 className="description">{item ? item.overview : ""}</h1>
+      </div>
+      <div className="fade_bottom"></div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const base_url = "https://api.themoviedb.org/3";
+export const API_KEY = "e28ec0f3e5914790e03405cb013f34d0";
+export const image = "https://image.tmdb.org/t/p/original";
