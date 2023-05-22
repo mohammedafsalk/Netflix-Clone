e8b1939be8ca658d6ee85ac2532f5e341b055c2f
mohammedafsalk <techafs456@gmail.com>
2023-05-22 17:45:03 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Navbar from "./Components/Navbar/Navbar";
 import './App.css'
+import Banner from "./Components/Banner/Banner";
+import RowPoster from "./Components/RowPoster/RowPoster";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Banner/>
+      <RowPoster/>
     </>
   );
 }
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './Banner.css'
 
 function Banner() {
   return (
-    <div>Banner</div>
+    <div className='banner'>
+        <div className="content">
+            <h1 className="title">Movie Name</h1>
+            <div className="banner_buttons">
+                <button className="button">Play</button>
+                <button className="button">My List</button>
+            </div>
+            <h1 className="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus odio quis ad odit praesentium itaque dolores vel! Velit sapiente veniam pariatur sunt recusandae id dignis</h1>
+        </div>
+        <div className="fade_bottom"></div>
+    </div>
   )
 }
 
